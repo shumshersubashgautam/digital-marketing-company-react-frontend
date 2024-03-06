2c97f16ac8b18ca397c1f1f284a48ae0d9e4fbdd
Subash Gautam <shumshersubash2018@gmail.com>
2024-03-06 08:02:07 +0545

    NavBar Completed without error issue

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar/>
     </div>
   );
 }
